9f5043fcdcac874b735894a2e234ef1e7ddee5fe
Uthaeus <romanlavery@gmail.com>
2023-04-05 16:44:54 -0600

    Implemented authorization for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
     def login_helper
-        if current_user.is_a?(User)
-            button_to 'Logout', destroy_user_session_path, method: :delete
-         else 
+        if current_user.is_a?(GuestUser)
             (link_to 'Log In', new_user_session_path ) +
             "<br>".html_safe +
             (link_to 'Sign Up', new_user_registration_path)
+         else 
+            button_to 'Logout', destroy_user_session_path, method: :delete
         end 
     end
 
