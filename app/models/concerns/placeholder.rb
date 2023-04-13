1f7405f320dec1107b444326d7b63fa9741f99b9
Uthaeus <romanlavery@gmail.com>
2023-04-13 09:42:23 -0600

    updated file upload forms and refactored placeholder fuctionality

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -1,5 +1,6 @@
 module Placeholder
-
+    # No longer being used
+    
     extend ActiveSupport::Concern
 
     def self.image_generator(height:, width:)
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,9 +5,8 @@ class Portfolio < ApplicationRecord
     accepts_nested_attributes_for :technologies, 
                                     reject_if: lambda { |attrs| attrs['name'].blank? }
 
-    include Placeholder
 
-    validates_presence_of :title, :body, :main_image, :thumb_image
+    validates_presence_of :title, :body
 
     mount_uploader :thumb_image, PortfolioUploader
     mount_uploader :main_image, PortfolioUploader
@@ -21,13 +20,6 @@ class Portfolio < ApplicationRecord
     end
 
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-
-    after_initialize :set_defaults
-
-    def set_defaults
-        self.main_image ||= Placeholder.image_generator(height: '400', width: '600')
-        self.thumb_image ||= Placeholder.image_generator(height: '200', width: '350')
-    end
 
 end
 
