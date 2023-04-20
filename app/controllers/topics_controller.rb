abd01db291a1088286d7736f694990d5a844a7cd
Uthaeus <romanlavery@gmail.com>
2023-04-19 21:59:43 -0600

    created new, create, and destroy actions for topics, added styling to topic index and item, adjusted styling for sidebar links

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,6 +6,22 @@ class TopicsController < ApplicationController
     @topics = Topic.all
   end
 
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to topics_path, notice: "Topic was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
     @topic = Topic.find(params[:id])
     
@@ -16,7 +32,20 @@ class TopicsController < ApplicationController
     end
   end
 
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+
+    respond_to do |format|
+      format.html { redirect_to topics_path, notice: "Blog was successfully destroyed." }
+    end
+  end
+
   private
+
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
 
   def set_sidebar_topics
     @side_bar_topics = Topic.with_blogs
